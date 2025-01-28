38b516a17d5a0a6cded72b5d2eac2654e025aaea
relliott-harness <78382621+relliott-harness@users.noreply.github.com>
2025-01-27 21:31:18 -0600

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,43 +1,43 @@
 import unittest
-from calc_robin import app
+from calc import calc
 
 class CalculatorTestCase(unittest.TestCase):
     def setUp(self):
-        self.app = app.test_client()
-        self.app.testing = True
+        self.calc = calc.test_client()
+        self.calc.testing = True
 
     def test_add(self):
-        response = self.app.post('/add', json={'x': 10, 'y': 5})
+        response = self.calc.post('/add', json={'x': 10, 'y': 5})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json['Message'], 15)
 
     def test_add_incorrect_params(self):
-        response = self.app.post('/add', json={'x': 10})
+        response = self.calc.post('/add', json={'x': 10})
         self.assertEqual(response.status_code, 301)
         self.assertEqual(response.json['Message'], "ERROR: incorrect number of paramters")
 
     def test_subtract(self):
-        response = self.app.post('/subtract', json={'x': 10, 'y': 5})
+        response = self.calc.post('/subtract', json={'x': 10, 'y': 5})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json['Message'], 5)
 
     def test_multiply(self):
-        response = self.app.post('/multiply', json={'x': 10, 'y': 5})
+        response = self.calc.post('/multiply', json={'x': 10, 'y': 5})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json['Message'], 50)
 
     def test_divide(self):
-        response = self.app.post('/divide', json={'x': 10, 'y': 5})
+        response = self.calc.post('/divide', json={'x': 10, 'y': 5})
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json['Message'], 2.0)
 
     def test_divide_by_zero(self):
-        response = self.app.post('/divide', json={'x': 10, 'y': 0})
+        response = self.calc.post('/divide', json={'x': 10, 'y': 0})
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.json['Message'], "ERROR: divide by zero")
 
     def test_divide_incorrect_params(self):
-        response = self.app.post('/divide', json={'x': 10})
+        response = self.calc.post('/divide', json={'x': 10})
         self.assertEqual(response.status_code, 301)
         self.assertEqual(response.json['Message'], "ERROR: incorrect number of paramters")
 
